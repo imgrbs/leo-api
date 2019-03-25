1d8e2252381eaf4e6fbb7a829ae7f32ecc490f1c
Keerati <keerati.jear@gmail.com>
2019-03-25 20:36:41 +0700

    [ADD] Add match repository

---
+++ MatchingService/src/main/java/app/leo/matching/repositories/MatchRepository.java
@@ -0,0 +1,9 @@
+package app.leo.matching.repositories;
+
+import app.leo.matching.models.Match;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MatchRepository extends JpaRepository<Match, Long> {
+}
