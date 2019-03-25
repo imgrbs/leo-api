474cd981ecd2028dc06d1d01f6d8403cfc684cc5
REi <Natchanon.minato@mail.kmutt.ac.th>
2019-03-26 02:19:21 +0700

    [ADD] MatchResult and Recruiter to MatchResult Method

--- MatchingService/src/main/java/app/leo/matching/models/MatchResult.java
+++ MatchingService/src/main/java/app/leo/matching/models/MatchResult.java
@@ -1,4 +1,31 @@
 package app.leo.matching.models;
 
+import app.leo.matching.demo.Applicant;
+import app.leo.matching.demo.Recruiter;
+
+import java.util.List;
+import java.util.Map;
+
 public class MatchResult {
+
+    private Map<Recruiter, List<Applicant>> result;
+
+    public MatchResult(Map<Recruiter, List<Applicant>> result) {
+        this.result = result;
+    }
+
+    public Map<Recruiter, List<Applicant>> getResult() {
+        return result;
+    }
+
+    public void setResult(Map<Recruiter, List<Applicant>> result) {
+        this.result = result;
+    }
+
+    @Override
+    public String toString() {
+        return "MatchResult{" +
+                "result=" + result +
+                '}';
+    }
 }
