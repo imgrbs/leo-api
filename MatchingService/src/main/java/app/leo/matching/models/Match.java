a46eaa7141a84e230c9e6efa0f3cb008326881e3
Keerati <keerati.jear@gmail.com>
2019-03-26 12:31:37 +0700

    [ADD] add in match has one to many applicant

--- MatchingService/src/main/java/app/leo/matching/models/Match.java
+++ MatchingService/src/main/java/app/leo/matching/models/Match.java
@@ -2,6 +2,7 @@ package app.leo.matching.models;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
+import java.util.List;
 
 @Entity
 @Table(name = "matches")
@@ -10,6 +11,9 @@ public class Match {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long matchId;
+
+    @OneToMany
+    private List<ApplicantMatch> applicantMatchList;
 
     public Match() {
     }
@@ -36,5 +40,13 @@ public class Match {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public List<ApplicantMatch> getApplicantMatchList() {
+        return applicantMatchList;
+    }
+
+    public void setApplicantMatchList(List<ApplicantMatch> applicantMatchList) {
+        this.applicantMatchList = applicantMatchList;
     }
 }
