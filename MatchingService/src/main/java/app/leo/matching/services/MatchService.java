319e7c445a8cf90a3d8dae7e5c05fefb4768dc70
Keerati <keerati.jear@gmail.com>
2019-03-27 01:52:59 +0700

    [ADD] implement matching algorithm that able to test !

--- MatchingService/src/main/java/app/leo/matching/services/MatchService.java
+++ MatchingService/src/main/java/app/leo/matching/services/MatchService.java
@@ -1,13 +1,15 @@
 package app.leo.matching.services;
 
-import app.leo.matching.models.ApplicantMatch;
-import app.leo.matching.models.Match;
+import app.leo.matching.models.*;
 import app.leo.matching.repositories.ApplicantMatchRepository;
 import app.leo.matching.repositories.MatchRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Service
 public class MatchService {
@@ -28,6 +30,88 @@ public class MatchService {
 
     public List<ApplicantMatch> getApplicantMatchByMatchId(long matchId) {
         return this.applicantMatchRepository.getApplicantMatchByMatchId(matchId);
+    }
+
+    public List<MatchResult> matching(long matchId) {
+        List<ApplicantMatch> applicantMatches = this.getApplicantMatchByMatchId(matchId);
+        List<MatchResult> matchResults = new ArrayList<MatchResult>();
+        Map<Position, List<ApplicantMatch>> positionAccepted = new HashMap<Position, List<ApplicantMatch>>();
+        while (!this.allApplicantMatched(applicantMatches)) {
+            ApplicantMatch applicant = applicantMatches.remove(0);
+            List<ApplicantRanking> applicantRanking = applicant.getApplicantRanking();
+            while (!applicantRanking.isEmpty()) {
+                Position position = applicantRanking.remove(0).getPosition();
+                List<ApplicantMatch> acceptedApplicant = positionAccepted.get(position);
+                if ((!this.isRecruiterHasFullCapacity(position, acceptedApplicant)) && this.isRecruiterAccepted(applicant, position.getRecruiterRankings())) {
+                    matchResults.add(new MatchResult(applicant, position));
+                    if (acceptedApplicant.isEmpty()) {
+                        acceptedApplicant = new ArrayList<>();
+                        acceptedApplicant.add(applicant);
+                    }
+                    positionAccepted.put(position, acceptedApplicant);
+                } else {
+                    int removalApplicantIndex = this.findRemovalApplicantInPositionRanking(position, applicant, positionAccepted);
+                    if (this.isRankBetterThanPositionAccepted(removalApplicantIndex)) {
+                        // remove old applicant accepted
+                        ApplicantMatch applicantMatch = acceptedApplicant.remove(removalApplicantIndex);
+                        // remove in match result
+                        matchResults = this.removeAcceptedApplicantInMatchResult(matchResults, removalApplicantIndex);
+                        // add new accepted
+                        positionAccepted.clear();
+                        positionAccepted.put(position, acceptedApplicant);
+                        // add new match result
+                        matchResults.add(new MatchResult(applicant, position));
+                        // add old accepted to applicantMatches with remove not accepted position
+                        applicantMatches.add(applicantMatch);
+                    } else {
+                        matchResults.add(new MatchResult(applicant, null));
+                    }
+                }
+            }
+        }
+        return matchResults;
+    }
+
+    public boolean allApplicantMatched(List<ApplicantMatch> applicants){
+        return applicants.isEmpty();
+    }
+
+
+    public boolean isRecruiterAccepted(ApplicantMatch applicant, List<RecruiterRanking> recruiterRanking){
+        if (recruiterRanking.contains(applicant)) {
+            return true;
+        }
+        return false;
+    }
+
+    public boolean isRecruiterHasFullCapacity(Position position, List<ApplicantMatch> applicantAccepted){
+        int capacity = position.getCapacity();
+        return applicantAccepted.size() == capacity || capacity <= 0;
+    }
+
+    public boolean isRankBetterThanPositionAccepted (int indexOfApplicant) {
+        return  indexOfApplicant > -1;
+    }
+
+    public int findRemovalApplicantInPositionRanking(Position position, ApplicantMatch applicant, Map<Position, List<ApplicantMatch>> positionAccepted){
+        int indexOfRemovalApplicant = -1;
+        List<RecruiterRanking> recruiterRankings = new ArrayList<RecruiterRanking>(position.getRecruiterRankings());
+        List<ApplicantMatch> applicantAccepted = new ArrayList<ApplicantMatch>(positionAccepted.get(position));
+        int indexOfNewApplicant = recruiterRankings.indexOf(applicant);
+        int newApplicantSequence = recruiterRankings.get(indexOfNewApplicant).getSequence();
+        for (ApplicantMatch applicantMatch: applicantAccepted) {
+            int indexOfOldApplicant = recruiterRankings.indexOf(applicantMatch);
+            int oldApplicantSequence = recruiterRankings.get(indexOfOldApplicant).getSequence();
+            if (oldApplicantSequence < newApplicantSequence) {
+                indexOfRemovalApplicant = indexOfOldApplicant;
+                break;
+            }
+        }
+        return indexOfRemovalApplicant ;
+    }
+
+    public List<MatchResult> removeAcceptedApplicantInMatchResult(List<MatchResult> matchResults, int removalApplicantIndex) {
+        return matchResults;
     }
 
 }
