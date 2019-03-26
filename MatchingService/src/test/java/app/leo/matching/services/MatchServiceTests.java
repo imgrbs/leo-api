32d0c3e03b853ebc70aa7b17509c84b4b05fe50a
Keerati <keerati.jear@gmail.com>
2019-03-26 11:40:32 +0700

    [ADD] Add get applicant match by match id with tests

--- MatchingService/src/test/java/app/leo/matching/services/MatchServiceTests.java
+++ MatchingService/src/test/java/app/leo/matching/services/MatchServiceTests.java
@@ -1,6 +1,9 @@
 package app.leo.matching.services;
 
+import app.leo.matching.models.ApplicantMatch;
+import app.leo.matching.models.ApplicantRanking;
 import app.leo.matching.models.Match;
+import app.leo.matching.repositories.ApplicantMatchRepository;
 import app.leo.matching.repositories.MatchRepository;
 
 import org.junit.Assert;
@@ -13,28 +16,64 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RunWith(MockitoJUnitRunner.class)
 public class MatchServiceTests {
+
+    private Match match;
+    private ApplicantMatch applicantMatch;
+    private List<ApplicantMatch> applicantMatchList;
+    private List<ApplicantRanking> applicantRankingList;
 
     private MatchService matchService;
 
     @Mock
     private MatchRepository matchRepository;
+    @Mock
+    private ApplicantMatchRepository applicantMatchRepository;
 
 
     @Before
     public void setUp() throws Exception {
+        this.match = new Match(1L, "SIT Career Day 2019");
+        this.applicantMatch = new ApplicantMatch(1L, true, this.match);
+        this.applicantMatchList = new ArrayList<ApplicantMatch>();
+        applicantMatchList.add(this.applicantMatch);
+
         this.matchRepository = Mockito.mock(MatchRepository.class);
-        this.matchService = new MatchService(this.matchRepository);
+        this.applicantMatchRepository = Mockito.mock(ApplicantMatchRepository.class);
+        this.matchService = new MatchService(this.matchRepository, this.applicantMatchRepository);
     }
 
     @Test
     public void findByIdShouldReturnValidMatchWhenMatchIdIsCorrect() {
-        Match match = new Match(1L, "SIT Career Day 2019");
-        Mockito.when(matchRepository.getMatchByMatchId(match.getMatchId())).thenReturn(match);
+        Mockito.when(matchRepository.getMatchByMatchId(this.match.getMatchId())).thenReturn(this.match);
 
-        Match matchMock = matchService.getMatchByMatchId(match.getMatchId());
+        Match matchMock = matchService.getMatchByMatchId(this.match.getMatchId());
 
-        Assert.assertEquals(matchMock.getName(), match.getName());
+        Assert.assertEquals(this.match.getName(), matchMock.getName());
+    }
+
+    @Test
+    public void getApplicantMatchByMatchIdShouldReturnDataCorrectly() {
+        Mockito.when(applicantMatchRepository.getApplicantMatchByMatchId(this.match.getMatchId())).thenReturn(this.applicantMatchList);
+
+        List<ApplicantMatch> applicantMatches = matchService.getApplicantMatchByMatchId(this.match.getMatchId());
+
+        Assert.assertEquals(1, applicantMatches.size());
+        Assert.assertEquals(this.applicantMatch, applicantMatches.get(0));
+        Assert.assertEquals(this.applicantMatch.getApplicantMatchId(), applicantMatches.get(0).getApplicantMatchId());
+    }
+
+    @Test
+    public void getApplicantMatchByInvalidMatchIdShouldReturnNull() {
+        long matchId = 2;
+        Mockito.when(applicantMatchRepository.getApplicantMatchByMatchId(matchId)).thenReturn(new ArrayList<>());
+
+        List<ApplicantMatch> applicantMatches = matchService.getApplicantMatchByMatchId(matchId);
+
+        Assert.assertEquals(0, applicantMatches.size());
     }
 }
