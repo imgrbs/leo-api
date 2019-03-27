532f9d02b04ca2a36f1842e2f873624529d0e8d3
Keerati <keerati.jear@gmail.com>
2019-03-27 11:12:22 +0700

    [UPDATE] refactor initial test data in match service tests

--- MatchingService/src/test/java/app/leo/matching/services/MatchServiceTests.java
+++ MatchingService/src/test/java/app/leo/matching/services/MatchServiceTests.java
@@ -1,5 +1,6 @@
 package app.leo.matching.services;
 
+import app.leo.matching.generators.ApplicantMatchGenerator;
 import app.leo.matching.models.*;
 import app.leo.matching.repositories.ApplicantMatchRepository;
 import app.leo.matching.repositories.MatchRepository;
@@ -20,58 +21,37 @@ import java.util.List;
 @RunWith(MockitoJUnitRunner.class)
 public class MatchServiceTests {
 
-    private Match match;
-    private ApplicantMatch applicantMatch;
-    private List<ApplicantMatch> applicantMatchList;
-    private List<ApplicantRanking> applicantRankingList;
-    private List<RecruiterMatch> recruiterMatches;
-    private List<Position> positions;
-    private Position position;
-    private MatchService matchService;
-    private ApplicantRanking applicantRanking;
-    private RecruiterRanking recruiterRanking;
-    private List<RecruiterRanking> recruiterRankingList;
-
     @Mock
     private MatchRepository matchRepository;
     @Mock
     private ApplicantMatchRepository applicantMatchRepository;
 
+    private MatchService matchService;
+
+    private ApplicantMatch applicantMatch1;
+    private Match match1;
+    private List<ApplicantMatch> applicantMatchListCase1;
+
+
+    private ApplicantMatch applicantMatch2;
+    private Match match2;
+    private List<ApplicantMatch> applicantMatchListCase2;
+
 
     @Before
     public void setUp() throws Exception {
-        this.match = new Match(1L, "SIT Career Day 2019");
-        this.applicantMatch = new ApplicantMatch(1L, true, this.match);
-        this.applicantMatchList = new ArrayList<ApplicantMatch>();
-        this.recruiterMatches = new ArrayList<RecruiterMatch>();
-        this.applicantRankingList = new ArrayList<ApplicantRanking>();
-        this.positions = new ArrayList<Position>();
-        this.recruiterRankingList = new ArrayList<RecruiterRanking>();
-        //first data
-        this.recruiterMatches.add(new RecruiterMatch(1L,this.match,true));
-        this.position = new Position("Data Engineer", 1,this.recruiterMatches.get(0));
-        this.applicantRanking = new ApplicantRanking(1,this.match,this.applicantMatch,position);
-        this.applicantRankingList.add(this.applicantRanking);
-        this.applicantMatch.setApplicantRanking(this.applicantRankingList);
-        applicantMatchList.add(this.applicantMatch);
-        this.recruiterRanking = new RecruiterRanking(1,this.match,this.position,applicantMatch);
-        this.recruiterRankingList.add(recruiterRanking);
-        this.position.setRecruiterRankings(recruiterRankingList);
+        this.match1 = new Match(1L, "SIT Career Day 2019");
+        this.applicantMatch1 = ApplicantMatchGenerator.generateApplicantMatch(match1, "Data Engineer", 1);
+        this.applicantMatchListCase1 = new ArrayList<ApplicantMatch>();
+        this.applicantMatchListCase1.add(applicantMatch1);
+        this.match1.setApplicantMatches(this.applicantMatchListCase1);
 
-        //second data
-        this.recruiterRankingList =new ArrayList<RecruiterRanking>();
-        this.position = new Position("Front-End Developer", 1 , this.recruiterMatches.get(0));
-        this.applicantMatch = new ApplicantMatch(2L, true, this.match);
-        this.applicantRanking = new ApplicantRanking(1,this.match,this.applicantMatch,position);
-        this.applicantRankingList.add(this.applicantRanking);
-        this.applicantMatch.setApplicantRanking(this.applicantRankingList);
-        applicantMatchList.add(this.applicantMatch);
-        this.recruiterRanking = new RecruiterRanking(2,this.match,this.position,applicantMatch);
-        this.recruiterRankingList.add(recruiterRanking);
-        this.position.setRecruiterRankings(recruiterRankingList);
+        this.match2 = new Match(2L, "KMUTT Job Fair");
+        this.applicantMatch2 = ApplicantMatchGenerator.generateApplicantMatch(match2, "Frontend-Developer", 1);
+        this.applicantMatchListCase2 = new ArrayList<ApplicantMatch>();
+        this.applicantMatchListCase2.add(applicantMatch2);
+        this.match2.setApplicantMatches(applicantMatchListCase2);
 
-
-        this.match.setApplicantMatches(this.applicantMatchList);
         this.matchRepository = Mockito.mock(MatchRepository.class);
         this.applicantMatchRepository = Mockito.mock(ApplicantMatchRepository.class);
         this.matchService = new MatchService(this.matchRepository, this.applicantMatchRepository);
@@ -79,29 +59,28 @@ public class MatchServiceTests {
 
     @Test
     public void findByIdShouldReturnValidMatchWhenMatchIdIsCorrect() {
-        Mockito.when(matchRepository.getMatchById(this.match.getId())).thenReturn(this.match);
+        Mockito.when(matchRepository.getMatchById(this.match1.getId())).thenReturn(this.match1);
 
-        Match matchMock = matchService.getMatchByMatchId(this.match.getId());
+        Match matchMock = matchService.getMatchByMatchId(this.match1.getId());
 
-        Assert.assertEquals(this.match.getName(), matchMock.getName());
+        Assert.assertEquals(this.match1.getName(), matchMock.getName());
     }
 
     @Test
     public void getApplicantMatchByMatchIdShouldReturnDataCorrectly() {
-        Mockito.when(applicantMatchRepository.getApplicantMatchByMatchId(this.match.getId())).thenReturn(this.applicantMatchList);
+        Mockito.when(applicantMatchRepository.getApplicantMatchByMatchId(this.match1.getId())).thenReturn(this.applicantMatchListCase1);
 
-        List<ApplicantMatch> applicantMatches = matchService.getApplicantMatchByMatchId(this.match.getId());
+        List<ApplicantMatch> applicantMatches = matchService.getApplicantMatchByMatchId(this.match1.getId());
 
         Assert.assertEquals(1, applicantMatches.size());
-        Assert.assertEquals(this.applicantMatch, applicantMatches.get(0));
-        Assert.assertEquals(this.applicantMatch.getId(), applicantMatches.get(0).getId());
+        Assert.assertEquals(this.applicantMatch1, applicantMatches.get(0));
+        Assert.assertEquals(this.applicantMatch1.getId(), applicantMatches.get(0).getId());
     }
 
     @Test
     public void getApplicantMatchByInvalidMatchIdShouldReturnNull() {
         long matchId = 2;
         Mockito.when(applicantMatchRepository.getApplicantMatchByMatchId(matchId)).thenReturn(new ArrayList<>());
-        System.out.println(this.applicantMatch);
         List<ApplicantMatch> applicantMatches = matchService.getApplicantMatchByMatchId(matchId);
 
         Assert.assertEquals(0, applicantMatches.size());
@@ -109,17 +88,15 @@ public class MatchServiceTests {
 
     @Test
     public void normalMatching(){
-        Mockito.when(applicantMatchRepository.getApplicantMatchByMatchId(this.match.getId())).thenReturn(this.applicantMatchList);
-        List<MatchResult> results =matchService.matching(this.match.getId());
+        Mockito.when(applicantMatchRepository.getApplicantMatchByMatchId(this.match1.getId())).thenReturn(this.applicantMatchListCase1);
+        List<MatchResult> results =matchService.matching(this.match1.getId());
 
         Assert.assertEquals(1,results.size());
     }
 
     @Test
     public void equalsTest(){
-        ApplicantMatch am = new ApplicantMatch(1L, true, this.match);
-        am.setApplicantRanking(this.applicantRankingList);
-        Assert.assertEquals(true,am.equals(this.applicantMatchList.get(0)));
+        Assert.assertEquals(true, this.applicantMatch1.equals(this.applicantMatchListCase1.get(0)));
     }
 
 }
