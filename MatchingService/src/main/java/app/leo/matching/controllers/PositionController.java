1c247de8246356ffca59c8bd416cf7bcc3a8dabd
Volku <Natchanon.minato@mail.kmutt.ac.th>
2019-03-30 08:23:22 +0700

    [ADD] add route and method getPositionByMatchId

--- MatchingService/src/main/java/app/leo/matching/controllers/PositionController.java
+++ MatchingService/src/main/java/app/leo/matching/controllers/PositionController.java
@@ -1,4 +1,23 @@
 package app.leo.matching.controllers;
 
+import app.leo.matching.models.Position;
+import app.leo.matching.services.PositionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
 public class PositionController {
+
+    @Autowired
+    private PositionService positionService;
+
+
+    @GetMapping(path= "match/{matchId:[\\d]}/position")
+    public List<Position> getPositionsByMatchId(@PathVariable long matchId){
+        return positionService.getPositionByMatchId(matchId);
+    }
 }
