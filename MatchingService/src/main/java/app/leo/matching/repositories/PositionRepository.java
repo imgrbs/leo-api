b156cf89916772ee8d27d3fdf0d298659f99c8eb
Volku <Natchanon.minato@mail.kmutt.ac.th>
2019-03-30 08:26:27 +0700

    [ADD] extend JpaRepository on PositionRepository and add findAllByMatchId to PositionRepository

--- MatchingService/src/main/java/app/leo/matching/repositories/PositionRepository.java
+++ MatchingService/src/main/java/app/leo/matching/repositories/PositionRepository.java
@@ -1,4 +1,14 @@
 package app.leo.matching.repositories;
 
-public class PositionRepository {
+import app.leo.matching.models.Position;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+
+@Repository
+public interface PositionRepository extends JpaRepository<Position,Long> {
+
+    public List<Position> findAllByMatchId(long matchId);
 }
