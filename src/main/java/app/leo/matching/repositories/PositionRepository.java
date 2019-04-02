429ff3ae74b9e81f005de1187a2e5b585df6729d
REi <Natchanon.minato@mail.kmutt.ac.th>
2019-04-02 14:15:35 +0700

    [Update] rename findAllBymatchId to getPositionbyMatchId and use NativeQuery

--- src/main/java/app/leo/matching/repositories/PositionRepository.java
+++ src/main/java/app/leo/matching/repositories/PositionRepository.java
@@ -2,6 +2,7 @@ package app.leo.matching.repositories;
 
 import app.leo.matching.models.Position;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,7 +11,8 @@ import java.util.List;
 @Repository
 public interface PositionRepository extends JpaRepository<Position,Long> {
 
-    public List<Position> findAllByMatchId(long matchId);
+    @Query(name = "select * from positions p where p.match_id = ?1 ", nativeQuery = true)
+    public List<Position> getPositionByMatchId(long matchId);
 
     public Position findPositionById(long Id);
 }
