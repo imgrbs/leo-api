562502dd625ff77828a75b43da5ec7b91d668626
REi <Natchanon.minato@mail.kmutt.ac.th>
2019-04-03 15:05:43 +0700

    [UPDATE] use @Valid to validate instead of try-catch in ApplicantRankingController

--- src/main/java/app/leo/matching/controllers/ApplicantRankingController.java
+++ src/main/java/app/leo/matching/controllers/ApplicantRankingController.java
@@ -1,18 +1,13 @@
 package app.leo.matching.controllers;
 
 
-import app.leo.matching.models.ApplicantMatch;
 import app.leo.matching.models.ApplicantRanking;
-import app.leo.matching.models.Match;
-import app.leo.matching.models.Position;
-import app.leo.matching.repositories.ApplicantMatchRepository;
-import app.leo.matching.repositories.PositionRepository;
 import app.leo.matching.services.ApplicantRankingService;
-import app.leo.matching.services.MatchService;
+import app.leo.matching.validator.CreateApplicantRankingRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Map;
+import javax.validation.Valid;
 
 
 @CrossOrigin("*")
@@ -23,22 +18,8 @@ public class ApplicantRankingController {
     private ApplicantRankingService applicantRankingService;
 
     @PostMapping(name= "matches/{matchId}/ranking")
-    public ApplicantRanking createApplicantRanking(@PathVariable long matchId,@RequestBody Map<String,Object> jsonBody){
-            try {
+    public ApplicantRanking createApplicantRanking(@PathVariable long matchId,@Valid @RequestBody CreateApplicantRankingRequest applicantRankingRequest){
                 long userId = 1;
-
-                int sequence = Integer.parseInt(jsonBody.get("sequence").toString());
-
-                if(sequence<=0){
-                    throw new Exception();
-                }
-
-                long positionId = Integer.parseInt(jsonBody.get("positionId").toString());
-                return applicantRankingService.createApplicantRanking(matchId,userId,positionId,sequence);
-
-            }catch(Exception e){
-                return null;
-            }
-
+                return applicantRankingService.createApplicantRanking(matchId,userId,applicantRankingRequest.getPositionId(),applicantRankingRequest.getSequence());
     }
 }
