786a560316cc02fadeaa7608de817f87c47dda75
REi <Natchanon.minato@mail.kmutt.ac.th>
2019-04-09 19:21:47 +0700

    [ADD] add findRecruiterRankingByPositionIdandApplicantId

--- src/main/java/app/leo/matching/repositories/RecruiterRankingRepository.java
+++ src/main/java/app/leo/matching/repositories/RecruiterRankingRepository.java
@@ -2,9 +2,12 @@ package app.leo.matching.repositories;
 
 import app.leo.matching.models.RecruiterRanking;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface RecruiterRankingRepository extends JpaRepository<RecruiterRanking,Long> {
 
+    @Query(name = "select rr from RecruiterRanking rr where rr.position_id = 1? and rr.applicant_match_id = 2?",nativeQuery = true)
+    public RecruiterRanking findRankingbyPositionIdandApplicantId(long positionId,long applicantMatchId);
 }
