a58827e2615d87f29875cb2e88ac2c0168e4c9e4
REi <Natchanon.minato@mail.kmutt.ac.th>
2019-04-09 19:23:20 +0700

    [ADD] add PutRecruiterRankingRequest

---
+++ src/main/java/app/leo/matching/validator/PutRecruiterRankingRequest.java
@@ -0,0 +1,31 @@
+package app.leo.matching.validator;
+
+public class PutRecruiterRankingRequest {
+
+    private long applicantId;
+    private int sequence;
+
+    public PutRecruiterRankingRequest() {
+    }
+
+    public PutRecruiterRankingRequest(long applicantId, int sequence) {
+        this.applicantId = applicantId;
+        this.sequence = sequence;
+    }
+
+    public long getApplicantId() {
+        return applicantId;
+    }
+
+    public void setApplicantId(long applicantId) {
+        this.applicantId = applicantId;
+    }
+
+    public int getSequence() {
+        return sequence;
+    }
+
+    public void setSequence(int sequence) {
+        this.sequence = sequence;
+    }
+}
