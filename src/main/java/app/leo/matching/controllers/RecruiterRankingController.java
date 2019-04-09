1763fa30cfeca62f490d92aa2af27fd81e2b2abc
REi <Natchanon.minato@mail.kmutt.ac.th>
2019-04-09 19:20:03 +0700

    [ADD] add Put Path updateRecruiterRanking

--- src/main/java/app/leo/matching/controllers/RecruiterRankingController.java
+++ src/main/java/app/leo/matching/controllers/RecruiterRankingController.java
@@ -4,16 +4,15 @@ package app.leo.matching.controllers;
 import app.leo.matching.models.RecruiterRanking;
 import app.leo.matching.services.RecruiterRankingService;
 import app.leo.matching.validator.CreateRecruiterRankingRequest;
+import app.leo.matching.validator.PutRecruiterRankingRequest;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.ArrayList;
 import java.util.List;
 
+@CrossOrigin("*")
 @RestController
 public class RecruiterRankingController {
 
@@ -25,6 +24,7 @@ public class RecruiterRankingController {
         long positionId = 1;
         createRecruiterRankingusingRequestList(matchId,positionId,createRecruiterRankingRequestList);
     }
+
 
     private List<RecruiterRanking> createRecruiterRankingusingRequestList(long matchId, long userId, List<CreateRecruiterRankingRequest> recruiterRankingRequests){
         List<RecruiterRanking> recruiterRankings = new ArrayList<RecruiterRanking>();
@@ -38,5 +38,26 @@ public class RecruiterRankingController {
             return recruiterRankings;
         }
         return recruiterRankings;
+    }
+
+    @PutMapping(path ="/matches/{matchId:[\\d]}/recruiters/ranking")
+    public List<RecruiterRanking> updateRecruiterRanking(@PathVariable long matchId, @Valid @RequestBody List<PutRecruiterRankingRequest> recruiterRankingRequestList){
+        long positionId = 1;
+        List<RecruiterRanking> putRecruiterRanking = new ArrayList<RecruiterRanking>();
+        for(PutRecruiterRankingRequest putRecruiterRankingRequest:recruiterRankingRequestList){
+            RecruiterRanking recruiterRanking =recruiterRankingService.findRecruiterRankingByPositionIdandApplicantId(positionId,putRecruiterRankingRequest.getApplicantId());
+            if(recruiterRanking != null){
+                recruiterRankingService.delectRecruiterRanking(recruiterRanking);
+            }else{
+                continue;
+            }
+            try {
+                recruiterRanking = recruiterRankingService.createRecruiterRanking(matchId, putRecruiterRankingRequest.getApplicantId(), positionId, putRecruiterRankingRequest.getSequence());
+                putRecruiterRanking.add(recruiterRanking);
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+        }
+      return putRecruiterRanking;
     }
 }
