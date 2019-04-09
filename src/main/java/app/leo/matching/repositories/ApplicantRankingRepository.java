09ad743c45751b2363bd593d50ca956da5eec8ad
REi <Natchanon.minato@mail.kmutt.ac.th>
2019-04-09 23:49:25 +0700

    [ADD] add query for find applicantRankingby PositionId and ApplicantId

--- src/main/java/app/leo/matching/repositories/ApplicantRankingRepository.java
+++ src/main/java/app/leo/matching/repositories/ApplicantRankingRepository.java
@@ -3,9 +3,12 @@ package app.leo.matching.repositories;
 
 import app.leo.matching.models.ApplicantRanking;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ApplicantRankingRepository extends JpaRepository<ApplicantRanking,Long> {
 
+    @Query(value= "select ar.* from applicant_rankings ar where ar.applicant_match_id = ?1 and ar.position_id = ?2" , nativeQuery = true)
+    ApplicantRanking getApplicantRankingByApplicantIdandPositionId(long applicantId,long positionId);
 }
