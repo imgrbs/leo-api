d72978766609696a81e07c4322a7479eb446a062
REi <Natchanon.minato@mail.kmutt.ac.th>
2019-04-09 23:48:30 +0700

    [UPDATE] update  putApplicantRanking path /matches/:id/applicants/ranking

--- src/main/java/app/leo/matching/controllers/ApplicantRankingController.java
+++ src/main/java/app/leo/matching/controllers/ApplicantRankingController.java
@@ -4,6 +4,7 @@ package app.leo.matching.controllers;
 import app.leo.matching.models.ApplicantRanking;
 import app.leo.matching.services.ApplicantRankingService;
 import app.leo.matching.validator.CreateApplicantRankingRequest;
+import app.leo.matching.validator.PutApplicantRankingRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -38,13 +39,31 @@ public class ApplicantRankingController {
         }
         return applicantRankingList;
     }
-    @PutMapping(path  = "/matches/{matchId:[\\d]}/ranking")
-    public ApplicantRanking  updateApplicantRanking(@PathVariable long matchId,@Valid @RequestBody ApplicantRanking applicantRanking){
-        return applicantRankingService.updateApplicantRanking(applicantRanking);
+    @PutMapping(path  = "/matches/{matchId:[\\d]}/applicants/ranking")
+    public List<ApplicantRanking> updateApplicantRanking(@PathVariable long matchId, @Valid @RequestBody List<PutApplicantRankingRequest> putApplicantRankingRequestList) {
+        long applicantId = 1;
+        List<ApplicantRanking> putApplicantRanking = new ArrayList<ApplicantRanking>();
+        for(PutApplicantRankingRequest putApplicantRankingRequest:putApplicantRankingRequestList) {
+            ApplicantRanking applicantRanking = applicantRankingService.getApplicantRankingByApplicantIdandPositionId(applicantId, putApplicantRankingRequest.getPositionId());
+            if (applicantRanking != null) {
+                applicantRankingService.deleteApplicantRanking(applicantRanking);
+            } else {
+                continue;
+            }
+            try {
+                applicantRanking = applicantRankingService.createApplicantRanking(matchId, applicantId, putApplicantRankingRequest.getPositionId(), putApplicantRankingRequest.getSequece());
+                putApplicantRanking.add(applicantRanking);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return putApplicantRanking;
     }
 
     @DeleteMapping(path = "/matches/{matchId:[\\d]}/ranking")
     public void deleteApplicantRanking(@PathVariable long matchId,@Valid @RequestBody ApplicantRanking applicantRanking){
-        applicantRankingService.deleteApplicantRanking(applicantRanking.getId());
+        applicantRankingService.deleteApplicantRankingById(applicantRanking.getId());
     }
+
+
 }
