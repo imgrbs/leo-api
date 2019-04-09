36e8a31f0696cf1043028d7b6eb03e0c4385e58d
REi <Natchanon.minato@mail.kmutt.ac.th>
2019-04-09 18:54:50 +0700

    [ADD] add putRecruiterToUnclarify

--- src/main/java/app/leo/matching/controllers/PositionController.java
+++ src/main/java/app/leo/matching/controllers/PositionController.java
@@ -3,18 +3,27 @@ package app.leo.matching.controllers;
 import app.leo.matching.models.Position;
 import app.leo.matching.services.PositionService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.Date;
 import java.util.List;
 
+@CrossOrigin("*")
 @RestController
 public class PositionController {
 
     @Autowired
     private PositionService positionService;
+
+    @PutMapping(path = "/matches/{matchId:[\\d]}/recruiters")
+    public void putRecruiterToUnclarify(@PathVariable long matchId,@Valid @RequestBody Long positionId) {
+        try {
+            positionService.putRecruiterToUnclarify(positionId);
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
 
     @GetMapping(path = "/healthcheck")
     public Date getDate(){
