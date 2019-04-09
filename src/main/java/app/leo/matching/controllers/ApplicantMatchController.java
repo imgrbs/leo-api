5fbde7d132f46e3103e1d685e38ccac8c582332f
REi <Natchanon.minato@mail.kmutt.ac.th>
2019-04-09 18:53:33 +0700

    [UPDATE] add CrossOrigin

--- src/main/java/app/leo/matching/controllers/ApplicantMatchController.java
+++ src/main/java/app/leo/matching/controllers/ApplicantMatchController.java
@@ -5,14 +5,12 @@ import app.leo.matching.models.RecruiterMatch;
 import app.leo.matching.repositories.ApplicantMatchRepository;
 import app.leo.matching.services.ApplicantMatchService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
 
+@CrossOrigin("*")
 @RestController
 public class ApplicantMatchController {
 
