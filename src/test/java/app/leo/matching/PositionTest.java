eb0309a7bdfb87518893884786a064f62055979d
REi <Natchanon.minato@mail.kmutt.ac.th>
2019-04-09 19:23:37 +0700

    [ADD] add PositionTest

---
+++ src/test/java/app/leo/matching/PositionTest.java
@@ -0,0 +1,53 @@
+package app.leo.matching;
+
+import app.leo.matching.models.ApplicantMatch;
+import app.leo.matching.models.Position;
+import app.leo.matching.models.RecruiterMatch;
+import app.leo.matching.models.RecruiterRanking;
+import app.leo.matching.repositories.PositionRepository;
+import app.leo.matching.services.PositionService;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
+public class PositionTest {
+    @Mock
+    private PositionRepository positionRepository;
+
+    private PositionService positionService;
+
+    private RecruiterMatch recruiter;
+    private Position position;
+    private RecruiterRanking recruiterRanking;
+    private ApplicantMatch applicantMatch;
+
+    @Before
+    public void setUp() throws Exception {
+        recruiter = new RecruiterMatch();
+        position = new Position("Programmer", 1, recruiter);
+        position.setId(1l);
+
+        positionRepository = mock(PositionRepository.class);
+        positionService = new PositionService(positionRepository);
+    }
+
+
+    @Test
+    public void putToUnclarifyPositionTest(){
+        when(positionRepository.findPositionById(this.position.getId())).thenReturn(position);
+        try {
+            positionService.putRecruiterToUnclarify(1l);
+            Assert.assertNotNull(position);
+            Assert.assertFalse(position.isIs_confirmation());
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+}
