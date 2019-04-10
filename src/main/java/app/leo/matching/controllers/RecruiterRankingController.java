04137390be5406166d9d59874e18c606a214ad19
REi <Natchanon.minato@mail.kmutt.ac.th>
2019-04-10 15:43:32 +0700

    [UPDATE] move update Recruiter ranking to Recruiter Ranking Service

--- src/main/java/app/leo/matching/controllers/RecruiterRankingController.java
+++ src/main/java/app/leo/matching/controllers/RecruiterRankingController.java
@@ -1,15 +1,15 @@
 package app.leo.matching.controllers;
 
 
-import app.leo.matching.models.RecruiterRanking;
 import app.leo.matching.services.RecruiterRankingService;
 import app.leo.matching.validator.CreateRecruiterRankingRequest;
 import app.leo.matching.validator.PutRecruiterRankingRequest;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-import java.util.ArrayList;
 import java.util.List;
 
 @CrossOrigin("*")
@@ -20,44 +20,20 @@ public class RecruiterRankingController {
     private RecruiterRankingService recruiterRankingService;
 
     @PostMapping("/matches/{matchId:[\\d]}/recruiters/ranking")
-    public void createRecruiterRanking(@PathVariable long matchId, @Valid @RequestBody List<CreateRecruiterRankingRequest>createRecruiterRankingRequestList){
+    public ResponseEntity<List<CreateRecruiterRankingRequest>>createRecruiterRanking(@PathVariable long matchId, @Valid @RequestBody List<CreateRecruiterRankingRequest>createRecruiterRankingRequestList){
         long positionId = 1;
-        createRecruiterRankingusingRequestList(matchId,positionId,createRecruiterRankingRequestList);
-    }
-
-
-    private List<RecruiterRanking> createRecruiterRankingusingRequestList(long matchId, long userId, List<CreateRecruiterRankingRequest> recruiterRankingRequests){
-        List<RecruiterRanking> recruiterRankings = new ArrayList<RecruiterRanking>();
-        for(CreateRecruiterRankingRequest recruiterRankingRequest:recruiterRankingRequests){
-            try {
-                RecruiterRanking recruiterRanking= recruiterRankingService.createRecruiterRanking(matchId,userId,recruiterRankingRequest.getApplicantId(),recruiterRankingRequest.getSequence());
-                recruiterRankings.add(recruiterRanking);
-            }catch (Exception e){
-                e.printStackTrace();
-            }
-            return recruiterRankings;
+        for(CreateRecruiterRankingRequest recruiterRankingRequest:createRecruiterRankingRequestList) {
+            recruiterRankingService.createRecruiterRanking(matchId,recruiterRankingRequest.getApplicantId(),positionId,recruiterRankingRequest.getSequence());
         }
-        return recruiterRankings;
+        return  new ResponseEntity<>(createRecruiterRankingRequestList,HttpStatus.CREATED);
     }
 
     @PutMapping(path ="/matches/{matchId:[\\d]}/recruiters/ranking")
-    public List<RecruiterRanking> updateRecruiterRanking(@PathVariable long matchId, @Valid @RequestBody List<PutRecruiterRankingRequest> recruiterRankingRequestList){
+    public ResponseEntity<List<PutRecruiterRankingRequest>> updateRecruiterRanking(@PathVariable long matchId, @Valid @RequestBody List<PutRecruiterRankingRequest> recruiterRankingRequestList){
         long positionId = 1;
-        List<RecruiterRanking> putRecruiterRanking = new ArrayList<RecruiterRanking>();
-        for(PutRecruiterRankingRequest putRecruiterRankingRequest:recruiterRankingRequestList){
-            RecruiterRanking recruiterRanking =recruiterRankingService.findRecruiterRankingByPositionIdandApplicantId(positionId,putRecruiterRankingRequest.getApplicantId());
-            if(recruiterRanking != null){
-                recruiterRankingService.delectRecruiterRanking(recruiterRanking);
-            }else{
-                continue;
-            }
-            try {
-                recruiterRanking = recruiterRankingService.createRecruiterRanking(matchId, putRecruiterRankingRequest.getApplicantId(), positionId, putRecruiterRankingRequest.getSequence());
-                putRecruiterRanking.add(recruiterRanking);
-            }catch (Exception e){
-                e.printStackTrace();
-            }
-        }
-      return putRecruiterRanking;
+        recruiterRankingService.updateRecuiterRankingByMatchIdAndPositionId(matchId,positionId,recruiterRankingRequestList);
+      return new ResponseEntity<>(recruiterRankingRequestList,HttpStatus.ACCEPTED);
     }
+
+
 }
--- src/main/java/app/leo/matching/repositories/RecruiterRankingRepository.java
+++ src/main/java/app/leo/matching/repositories/RecruiterRankingRepository.java
@@ -5,9 +5,19 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import javax.transaction.Transactional;
+import java.util.List;
+
 @Repository
 public interface RecruiterRankingRepository extends JpaRepository<RecruiterRanking,Long> {
 
     @Query(value = "select rr.* from recruiter_rankings rr where rr.position_id = ?1 and rr.applicant_match_id = ?2",nativeQuery = true)
     RecruiterRanking getRecruiterRankingbyPositionIdandApplicantMatchId(long positionId,long applicantMatchId);
+
+    @Transactional
+    void deleteRecruiterRankingByMatchIdAndPositionId(long matchId, long positionId);
+
+    List<RecruiterRanking> getRecruiterRankingByMatchIdAndPositionId(long matchId,long positionId);
+
+
 }
