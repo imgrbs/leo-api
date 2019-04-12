60f62d6688e1e44c528cc564b9f081ffcb1e992e
Volku <Natchanon.minato@mail.kmutt.ac.th>
2019-04-12 17:55:38 +0700

    [UPDATE] update get Applicant by position Id route

--- src/main/java/app/leo/matching/controllers/ApplicantMatchController.java
+++ src/main/java/app/leo/matching/controllers/ApplicantMatchController.java
@@ -5,9 +5,11 @@ import app.leo.matching.models.RecruiterMatch;
 import app.leo.matching.repositories.ApplicantMatchRepository;
 import app.leo.matching.services.ApplicantMatchService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import javax.validation.Valid;
+
 import java.util.List;
 
 @CrossOrigin("*")
@@ -18,8 +20,8 @@ public class ApplicantMatchController {
     private ApplicantMatchService applicantMatchService;
 
 
-    @GetMapping("/matches/{matchId:[\\d]}/recruiters/applicants")
-    public List<ApplicantMatch> getApplicantMatchesByPositionId(@PathVariable long matchId,@Valid @RequestBody RecruiterMatch recruiterMatch){
-        return applicantMatchService.getApplicantMatchesByMatchIdandRecruiterMatchId(matchId,recruiterMatch);
+    @GetMapping(path = "/matches/{matchId:[\\d]}/positions/{positionId:[\\d]}/applicants")
+    public ResponseEntity<List<ApplicantMatch>> getApplicantMatchesByPositionId(@PathVariable long matchId, @PathVariable long positionId){
+        return new ResponseEntity<>(applicantMatchService.getApplicantMatchesByMatchIdandPositionId(matchId,positionId), HttpStatus.OK);
     }
 }
