cff15ea04809c645a5e4b5742ed9ac85c0e2509a
Volku <Natchanon.minato@mail.kmutt.ac.th>
2019-04-13 23:48:44 +0700

    [UPDATE] update query for get Applicant Match by position id

--- src/main/java/app/leo/matching/repositories/ApplicantMatchRepository.java
+++ src/main/java/app/leo/matching/repositories/ApplicantMatchRepository.java
@@ -13,6 +13,6 @@ public interface ApplicantMatchRepository extends JpaRepository<ApplicantMatch, 
 
     ApplicantMatch getApplicantMatchById(long id);
 
-    @Query(value = "select a.id , a.applicant_id from applicant_matches a inner join applicant_rankings ar on a.id = ar.applicant_match_id where a.match_id = ?1 and ar.position_id = ?2" , nativeQuery = true)
+    @Query(value = "select a.id , a.applicant_id ,a.match_id from applicant_matches a inner join applicant_rankings ar on a.id = ar.applicant_match_id where a.match_id = ?1 and ar.position_id = ?2" , nativeQuery = true)
     List<ApplicantMatch> getApplicantMatchByMatchIdAndPositionId(Long matchId,Long positionId);
 }
