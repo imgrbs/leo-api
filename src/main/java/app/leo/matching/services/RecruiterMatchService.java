e3d8cc75dff343984221a36babf5181419dd7de1
REi <Natchanon.minato@mail.kmutt.ac.th>
2019-04-24 03:10:52 +0700

    [ADD] add RecruiterMatchService file and add Method get RecruiterMatch By Recruiter Id

---
+++ src/main/java/app/leo/matching/services/RecruiterMatchService.java
@@ -0,0 +1,19 @@
+package app.leo.matching.services;
+
+import app.leo.matching.models.RecruiterMatch;
+import app.leo.matching.repositories.RecruiterMatchRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class RecruiterMatchService {
+
+    @Autowired
+    private RecruiterMatchRepository recruiterMatchRepository;
+
+    public List<RecruiterMatch> getRecruiterMatchByRecruiterId(long recruiterId){
+       return recruiterMatchRepository.getRecruiterMatchesByRecruiterId(recruiterId);
+    }
+}
