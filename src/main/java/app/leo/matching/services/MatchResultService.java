c9e0e28a0870eff9ad6cdbf8e37fe2f28f847693
REi <Natchanon.minato@mail.kmutt.ac.th>
2019-04-24 16:11:12 +0700

    [ADD] add Match Result Service

---
+++ src/main/java/app/leo/matching/services/MatchResultService.java
@@ -0,0 +1,21 @@
+package app.leo.matching.services;
+
+import app.leo.matching.models.MatchResult;
+import app.leo.matching.repositories.MatchResultRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MatchResultService {
+
+    @Autowired
+    private MatchResultRepository matchResultRepository;
+
+    public MatchResult getMatchResultByApplicantMatchIdAndMatchId(long applicantMatchId,long matchId){
+        return matchResultRepository.getMatchResultByApplicantMatch_IdAndMatchId(applicantMatchId,matchId);
+    }
+
+    public MatchResult getMatchResultByPositionIdAndMatchId(long positionId,long matchId){
+        return matchResultRepository.getMatchResultByPosition_IdAndMatchId(positionId,matchId);
+    }
+}
