f8e52236d7b1b3a3bd49e43f5453b519d7d02853
REi <Natchanon.minato@mail.kmutt.ac.th>
2019-04-25 14:15:30 +0700

    [UPDATE] update get Applicant Ranking to use Get Ranking Response and use mock data

--- src/main/java/app/leo/matching/controllers/ApplicantRankingController.java
+++ src/main/java/app/leo/matching/controllers/ApplicantRankingController.java
@@ -1,16 +1,19 @@
 package app.leo.matching.controllers;
 
 
+import app.leo.matching.DTO.*;
 import app.leo.matching.models.ApplicantRanking;
 import app.leo.matching.services.ApplicantRankingService;
 import app.leo.matching.validator.CreateApplicantRankingRequest;
 import app.leo.matching.validator.PutApplicantRankingRequest;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.ArrayList;
 import java.util.List;
 
 
@@ -31,9 +34,37 @@ public class ApplicantRankingController {
     }
 
     @GetMapping(path= "/matches/{matchId:[\\d+]}/applicants/ranking")
-    public ResponseEntity<List<ApplicantRanking>> getApplicantRanking(@Valid @PathVariable("matchId") Long matchId){
+    public ResponseEntity<List<GetRankingResponse>> getApplicantRanking(@Valid @PathVariable("matchId") Long matchId){
         long userId = 1;
-        return new ResponseEntity<>(applicantRankingService.getApplicantRankingByMatchIdAndApplicantId(matchId, userId), HttpStatus.OK);
+        return new ResponseEntity<>(mapApplicantRankingtoResponse(applicantRankingService.getApplicantRankingByMatchIdAndApplicantId(matchId, userId)), HttpStatus.OK);
+    }
+
+    private List<GetRankingResponse> mapApplicantRankingtoResponse(List<ApplicantRanking> applicantRankingList){
+        ModelMapper modelMapper =new ModelMapper();
+        List<GetRankingResponse> responses = new ArrayList<>();
+        for(ApplicantRanking applicantRanking:applicantRankingList){
+            GetRankingResponse getRankingResponse = modelMapper.map(applicantRanking,GetRankingResponse.class);
+            GetPositionsByMatchIdResponse position  = getRankingResponse.getPosition();
+            mockRecruiterInstall(position);
+            GetApplicantsByMatchIdResponse applicant = getRankingResponse.getApplicantMatch();
+            mockApplicantInstall(applicant);
+            getRankingResponse.setApplicantMatch(applicant);
+            getRankingResponse.setPosition(position);
+            responses.add(getRankingResponse);
+        }
+        return  responses;
+    }
+
+    private void mockRecruiterInstall(GetPositionsByMatchIdResponse position){
+        Recruiter recruiter = new Recruiter(1L,"Microsoft word co., Ltd","Phayathai, BKK");
+        position.setRecruiter(recruiter);
+    }
+
+    private void mockApplicantInstall(GetApplicantsByMatchIdResponse applicant){
+        List<Education> educations = new ArrayList<>();
+        educations.add(new Education(1, "School of Information Technology", "4.00"));
+        Applicant mockApplicant = new Applicant(1, "Tae Keerati", educations);
+        applicant.setApplicant(mockApplicant);
     }
 
     @PutMapping(path  = "/matches/{matchId:[\\d]}/applicants/ranking")
