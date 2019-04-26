3d572cb96b7db34199dc73ae367f8dc6820041e2
Keerati <keerati.jear@gmail.com>
2019-04-26 23:35:41 +0700

    [ADD] add matching route for matching by match id

---
+++ src/main/java/app/leo/matching/controllers/MatchingController.java
@@ -0,0 +1,24 @@
+package app.leo.matching.controllers;
+
+import app.leo.matching.models.MatchResult;
+import app.leo.matching.services.MatchingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class MatchingController {
+
+    @Autowired
+    MatchingService matchingService;
+
+    @PostMapping("/matches/{matchId:[\\d]}/matching")
+    public ResponseEntity<List<MatchResult>>createRecruiterRanking(@PathVariable long matchId){
+        return  new ResponseEntity<>(matchingService.matchingByMatchId(matchId), HttpStatus.CREATED);
+    }
+}
