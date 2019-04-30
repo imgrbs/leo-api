b27f02f6bda79f697b65f89419b3cdac3bbb86ba
Keerati <keerati.jear@gmail.com>
2019-04-30 11:12:59 +0700

    [UPDATE] update query for get match result by position id and match id to return list

--- src/main/java/app/leo/matching/repositories/MatchResultRepository.java
+++ src/main/java/app/leo/matching/repositories/MatchResultRepository.java
@@ -4,10 +4,12 @@ import app.leo.matching.models.MatchResult;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface MatchResultRepository extends JpaRepository<MatchResult,Long> {
 
     MatchResult getMatchResultByApplicantMatchIdAndMatchId(long applicantMatchId,long matchId);
 
-    MatchResult getMatchResultByPositionIdAndMatchId(long positionId , long matchId);
+    List<MatchResult> getMatchResultByPositionIdAndMatchId(long positionId , long matchId);
 }
