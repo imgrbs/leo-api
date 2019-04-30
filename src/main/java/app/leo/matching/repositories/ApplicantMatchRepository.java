6c8a0499eb2b6d4b0f6f2310c82fac512f24c939
Keerati <keerati.jear@gmail.com>
2019-04-30 11:13:52 +0700

    [ADD] add query to get applicant by applicant id

--- src/main/java/app/leo/matching/repositories/ApplicantMatchRepository.java
+++ src/main/java/app/leo/matching/repositories/ApplicantMatchRepository.java
@@ -14,6 +14,8 @@ public interface ApplicantMatchRepository extends JpaRepository<ApplicantMatch, 
 
     ApplicantMatch getApplicantMatchById(long id);
 
+    ApplicantMatch getApplicantMatchByApplicantIdAndMatchId(long applicantId, long matchId);
+
     @Query(value = "select a.id , a.applicant_id ,a.match_id from applicant_matches a inner join applicant_rankings ar on a.id = ar.applicant_match_id where a.match_id = ?1 and ar.position_id = ?2" , nativeQuery = true)
     List<ApplicantMatch> getApplicantMatchByMatchIdAndPositionId(Long matchId,Long positionId);
 
