a76a45f8a2a1a647f6bf6ad58756c84717e069ba
Keerati <keerati.jear@gmail.com>
2019-05-15 00:48:50 +0700

    [UPDATE] fix error by update query to get join_date

--- src/main/java/app/leo/matching/repositories/ApplicantMatchRepository.java
+++ src/main/java/app/leo/matching/repositories/ApplicantMatchRepository.java
@@ -16,7 +16,10 @@ public interface ApplicantMatchRepository extends JpaRepository<ApplicantMatch, 
 
     ApplicantMatch getApplicantMatchByApplicantIdAndMatchId(long applicantId, long matchId);
 
-    @Query(value = "select DISTINCT(a.participant_id) , a.applicant_id ,a.match_id from leo.applicant_matches a inner join leo.applicant_rankings ar on a.participant_id= ar.applicant_match_id where a.match_id = ?1 and ar.position_id = ?2" , nativeQuery = true)
+    @Query(value = "select DISTINCT(a.participant_id), a.applicant_id, a.match_id, a.join_date " +
+            "from applicant_matches a " +
+            "inner join applicant_rankings ar on a.participant_id = ar.applicant_match_id " +
+            "where a.match_id = ?1 and ar.position_id = ?2" , nativeQuery = true)
     List<ApplicantMatch> getApplicantMatchByMatchIdAndPositionId(Long matchId,Long positionId);
 
     List<ApplicantMatch> getApplicantMatchByApplicantId(long applicantId);
