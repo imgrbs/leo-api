0ed79c596648dc37942b2b531a1c2f9b09af19fe
Keerati <keerati.jear@gmail.com>
2019-08-13 11:17:39 +0700

    [REMOVE] remove try catch for thrown only

--- src/main/java/app/leo/matching/adapters/MatchManagementAdapter.java
+++ src/main/java/app/leo/matching/adapters/MatchManagementAdapter.java
@@ -1,44 +1,41 @@
 package app.leo.matching.adapters;
 
+import java.util.Arrays;
 
-import app.leo.matching.DTO.MatchDTO;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.*;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.Arrays;
+import app.leo.matching.DTO.MatchDTO;
 
 @Service
 public class MatchManagementAdapter {
-    @Value("${MatchManagement_API_URL}")
-    private String matchManagementApiUrl;
+	@Value("${MatchManagement_API_URL}")
+	private String matchManagementApiUrl;
 
-    private Logger logger = LoggerFactory.getLogger(MatchManagementAdapter.class);
-    public MatchDTO getMatchByMatchId(String token,long matchId){
-        MatchDTO match = null;
-        try {
-            RestTemplate restTemplate = new RestTemplate();
-            String url = String.format(matchManagementApiUrl + "matches/" + matchId);
-            HttpHeaders headers = new HttpHeaders();
-            headers.add("Authorization", token);
-            headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
-            headers.add("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36");
-            HttpEntity<String> entity = new HttpEntity<>(headers);
-            System.out.println(entity.getHeaders());
-            ResponseEntity<MatchDTO> responseEntity =restTemplate.exchange(
-                    url,
-                    HttpMethod.GET,
-                    entity,
-                    new ParameterizedTypeReference<MatchDTO>(){}
-            );
-            match = responseEntity.getBody();
-        }catch (NullPointerException exception){
-            logger.warn(exception.getMessage());
-        }
-        return match;
-    }
+	private Logger logger = LoggerFactory.getLogger(MatchManagementAdapter.class);
+
+	public MatchDTO getMatchByMatchId(String token, long matchId) {
+		RestTemplate restTemplate = new RestTemplate();
+		String url = String.format(matchManagementApiUrl + "matches/" + matchId);
+		HttpHeaders headers = new HttpHeaders();
+		headers.add("Authorization", token);
+		headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+		headers.add("user-agent",
+				"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36");
+		HttpEntity<String> entity = new HttpEntity<>(headers);
+		System.out.println(entity.getHeaders());
+		ResponseEntity<MatchDTO> responseEntity = restTemplate.exchange(url, HttpMethod.GET, entity,
+				new ParameterizedTypeReference<MatchDTO>() {
+				});
+		return responseEntity.getBody();
+	}
 }
