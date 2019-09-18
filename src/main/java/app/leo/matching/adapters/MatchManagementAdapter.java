6e94d1518b71edc49369407f38800e8193549670
REi <Natchanon.minato@mail.kmutt.ac.th>
2019-09-19 01:11:55 +0700

    [UPDATE] add Bearer to Authentication Header

--- src/main/java/app/leo/matching/adapters/MatchManagementAdapter.java
+++ src/main/java/app/leo/matching/adapters/MatchManagementAdapter.java
@@ -27,7 +27,7 @@ public class MatchManagementAdapter {
 		RestTemplate restTemplate = new RestTemplate();
 		String url = String.format(matchManagementApiUrl + "matches/" + matchId);
 		HttpHeaders headers = new HttpHeaders();
-		headers.add("Authorization", token);
+		headers.add("Authorization", "Bearer " + token);
 		headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
 		headers.add("user-agent",
 				"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36");
