5ac277903a7bc58c6a1c92020302192f5727bdeb
REi <Natchanon.minato@mail.kmutt.ac.th>
2019-11-18 04:08:47 +0700

    [UPDATE] check user role when call matching method

--- src/main/java/app/leo/matching/controllers/MatchingController.java
+++ src/main/java/app/leo/matching/controllers/MatchingController.java
@@ -1,5 +1,7 @@
 package app.leo.matching.controllers;
 
+import app.leo.matching.DTO.User;
+import app.leo.matching.exceptions.WrongRoleException;
 import app.leo.matching.models.MatchResult;
 import app.leo.matching.services.MatchingService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +9,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -17,8 +20,12 @@ public class MatchingController {
     @Autowired
     MatchingService matchingService;
 
+    private final String ORGANIZER_ROLE = "organizer";
     @PostMapping("/matches/{matchId}/matching")
-    public ResponseEntity<List<MatchResult>>createRecruiterRanking(@PathVariable long matchId){
-        return  new ResponseEntity<>(matchingService.matchingByMatchId(matchId), HttpStatus.CREATED);
+    public ResponseEntity<List<MatchResult>>createRecruiterRanking(@PathVariable long matchId, @RequestAttribute("user") User user){
+        if(user.getRole().equals(ORGANIZER_ROLE)) {
+            return new ResponseEntity<>(matchingService.matchingByMatchId(matchId), HttpStatus.CREATED);
+        }
+        throw new WrongRoleException("You are not organizer, You can not call matching");
     }
 }
